0a01dd3f8d0fe6bce106c24968f9327745581578
iappotakebee <sakuya3.1341.popnmusic@gmail.com>
2021-11-12 17:19:23 +0900

    [update] updated the timetable and the registration form

--- js/timetable.js
+++ js/timetable.js
@@ -41,8 +41,7 @@ function construstTimeTable(timeTable, talksData) {
 
       ) {
         return { time: time, name: talk.affiliation, 
-          title: talk.title + `：
-          ` + talk.name, 
+          title: talk.title + "：" + talk.name, 
           major: "" };
       }
       //else if (name == "第1部講演の録画を放映予定") {
